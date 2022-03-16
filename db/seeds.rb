619f0eaaa46f404748e1e97dcfeef4afb53a30f5
Patrick Almeida <patrick.c.almeida@gmail.com>
2022-03-16 14:43:26 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Epicure", category: "french", address: "Paris", phone_number: "79887652525")
+Restaurant.create(name: "Frites", category: "belgian", address: "Ours", phone_number: "79885")
+Restaurant.create(name: "huchitomi", category: "japanese", address: "Lausanne", phone_number: "02188876578")
+Restaurant.create(name: "xau xi", category: "chinese", address: "Morges", phone_number: "098887525")
+Restaurant.create(name: "Luigia", category: "italian", address: "Lausanne", phone_number: "52525")
